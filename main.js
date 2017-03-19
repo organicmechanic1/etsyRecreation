d37b31dbe2ca2fc64a57eec070f5b1b1280b5f2d
Erick G <lmterickg@cox.net>
2017-03-19 16:18:37 -0700

    gallery complete

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 $(document).ready(function (){
 
-
-
+$.getJSON('https://api.etsy.com/v2/listings/active.js?api_key=h9oq2yf3twf4ziejn10b717i&keywords=whiskey&includes=Images,Shop&callback=?', function(data){
+console.log(data)
 
 
 
@@ -22,4 +22,10 @@ $(document).ready(function (){
 
 
 	
+
+
+
+
+	})
+
 })
